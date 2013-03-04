ebddbe43390709371f76f5066d34163efdcc7038
Lur Ibargutxi <libargutxi@codesyntax.com>
2013-03-04 16:35:13 +0100

    delete imports

--- cs/kontratazioa/interfaces/kontratazioafolder.py
+++ cs/kontratazioa/interfaces/kontratazioafolder.py
@@ -1,9 +1,6 @@
 from zope import schema
 from zope.interface import Interface
 
-from zope.app.container.constraints import contains
-from zope.app.container.constraints import containers
-
 from cs.kontratazioa import kontratazioaMessageFactory as _
 
 class IkontratazioaFolder(Interface):
